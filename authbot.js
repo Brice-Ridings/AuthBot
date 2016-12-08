f3dd228daf199d860f887b2d6fea486ac8ac9c30
Brice-Ridings <Brice.M.Ridings@gmail.com>
2016-12-07 20:31:44 -0500

    added delete and refresh functions

--- authbot.js
+++ authbot.js
@@ -34,10 +34,64 @@ controller.hears(['hello','hi'],['direct_message','direct_mention','mention'],fu
     });
 });
 
+controller.hears('do my laundry',['direct_message','direct_mention','mention'],function(bot,message){
+  bot.reply(message, 'Here you are :shirt: :shirt: :jeans:');
+});
+
 controller.hears(['help','who are you','what do you do', 'identify yourself'], ['direct_message','direct_mention','mention'], function(bot,message){
-  bot.reply(message,':robot_face:I am authbot, a slackbot built for handling auth0 accounts within the Inmar custom database.\n'
-  +'Currently I only have one job so I\'ll likely not fail!\n'
-  +'Just tell me to \"*create account .....*\" and I\'ll create a new dev auth0 account');
+  bot.reply(message,'I am authbot, a slackbot built for handling auth0 accounts within the Inmar custom database.\n'
+  +'Hear are some things you can command me to do\n'
+  +'*create account xxxxx-api/wkr*\n' + '>creates an account for APIs or workers in Auth0 inmarb2b-dev database. Names must end with "-wkr" or "-api" to be accepted. I append "dev-" to what you provide\n'
+  +'*create service account xxxxxx*\n' + '>This is for accounts that are not APIs or workers. I will append "svc-" at the beggining\n'
+  +'*delete account xxxxx*\n' + '>deletes the account of course\n'
+  +'*refresh account xxxx*\n' + '>if this account exists I will refresh the password');
+});
+
+controller.hears('delete account (.*)',['direct_message','direct_mention','mention'],function(bot,message){
+  var account = message.match[1];
+  bot.reply(message, "Sure! one sec");
+
+  var createAccountOptions = {
+    url: config.urlEdit + account,
+    method: 'Delete'
+  };
+
+  request(createAccountOptions, function(error, response, body){
+    if(!error && response.statusCode == '200'){
+      bot.reply(message, 'The account has been deleted');
+    }
+    else if(response.statusCode == 504 || response.statusCode == 404){
+      bot.reply(message,"Well this is embarrassing... Seems the service is unavaliable and I recieved a status code of " + response.statusCode);
+    }
+    else{
+      bot.reply(message,"Error "+response.statusCode+": "+JSON.parse(body));
+    }
+  })
+
+});
+
+controller.hears('refresh account (.*)',['direct_message','direct_mention','mention'],function(bot,message){
+  var account = message.match[1];
+  bot.reply(message, "Of course! Please hold on to your password :upside_down_face:");
+
+  var createAccountOptions = {
+    url: config.urlEdit + account,
+    method: 'Put'
+  };
+
+  request(createAccountOptions, function(error, response, body){
+    if(!error && response.statusCode == '201'){
+      var responseBody = JSON.parse(body);
+      bot.reply(message, 'Here you are!' +  '\n' + '>>> AccountName: ' + responseBody.account.appName + '\n' + 'Password: ' + responseBody.account.password);
+    }
+    else if(response.statusCode == 504 || response.statusCode == 404){
+      bot.reply(message,"Well this is embarrassing... Seems the service is unavaliable and I recieved a status code of " + response.statusCode);
+    }
+    else{
+      bot.reply(message,"Error "+response.statusCode+": "+JSON.parse(body));
+    }
+  })
+
 });
 
 // Create account xxxxxx
@@ -48,7 +102,7 @@ controller.hears('create account (.*)', ['direct_message','direct_mention','ment
   // Check to see if account matches the expected value...
   if(account.match(accountReq)){
 
-    bot.reply(message, "Got it! one moment...")
+    bot.reply(message, "Got it! one moment...");
     accountName = 'dev-' + account;
 
     // Create request call
